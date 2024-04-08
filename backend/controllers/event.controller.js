4aef598399fea4a71086e131492df252b9936b2c
GemboJones <gembojones@gmail.com>
2024-04-08 23:32:55 +0100

    adds update event and update attendance endpoints

--- backend/controllers/event.controller.js
+++ backend/controllers/event.controller.js
@@ -1,7 +1,6 @@
 const Event = require("../models/event.model.js");
 
-
-exports.getAllEvents = async (req, res) => {
+const getAllEvents = async (req, res) => {
   try {
     const events = await Event.find({});
     res.status(200).send(events);
@@ -10,9 +9,9 @@ exports.getAllEvents = async (req, res) => {
   }
 };
 
-exports.getEvent = async (req, res) => {
+const getEvent = async (req, res) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
     const event = await Event.findById(id);
     res.status(200).send(event);
   } catch (error) {
@@ -20,13 +19,41 @@ exports.getEvent = async (req, res) => {
   }
 };
 
-exports.createEvent = async (req, res) => {
+const createEvent = async (req, res) => {
   try {
     const event = await Event.create(req.body);
-    console.log(req.body);
     res.status(200).send(event);
-    console.log(event);
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
-}
+};
+
+const updateEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const event = await Event.findByIdAndUpdate(id, req.body);
+    if (!event) {
+      return res.status(404).send({ message: "Event not found" });
+    }
+    const updatedEvent = await Event.findById(id);
+    res.status(200).send(updatedEvent);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+const updateAttending = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const event = await Event.findByIdAndUpdate(id, {$addToSet: req.body});
+    if (!event) {
+      return res.status(404).send({ message: "Event not found" });
+    }
+    const updatedEvent = await Event.findById(id);
+    res.status(200).send(updatedEvent);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+module.exports = { getAllEvents, getEvent, createEvent, updateEvent, updateAttending };
--- backend/routes/event.route.js
+++ backend/routes/event.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getAllEvents, getEvent, createEvent } = require("../controllers/event.controller.js")
+const { getAllEvents, getEvent, createEvent, updateEvent, updateAttending } = require("../controllers/event.controller.js")
 
 
 router.get("/", getAllEvents);
 router.get("/:id", getEvent);
 router.post("/", createEvent);
+router.patch("/:id", updateEvent);
+router.patch("/:id/attending", updateAttending);
 
 module.exports = router;
