dab36180b048ad9fd90b07f2a38ffac23d84c98a
GemboJones <gembojones@gmail.com>
2024-04-15 00:25:14 +0100

    adds conditional admin vs member views in navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { NavLink, Link } from "react-router-dom";
+import UserContext from "../context/userContext";
 import styles from "../styles/Navbar.module.css";
-import closeIcon from "../assets/closeIcon.png"
-import menuIcon from "../assets/menuIcon.png"
+import closeIcon from "../assets/closeIcon.png";
+import menuIcon from "../assets/menuIcon.png";
 
 export const Navbar = () => {
+  const { user, setUser } = useContext(UserContext);
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
@@ -26,15 +29,32 @@ export const Navbar = () => {
           <li>
             <NavLink to="/events">Events</NavLink>
           </li>
+          {user.role === "admin" && 
           <li>
             <NavLink to="/create">Create Event</NavLink>
           </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-          <li>
-            <NavLink to="/signup">Signup</NavLink>
-          </li>
+          }
+          {user ? (
+            <li>
+              <NavLink
+                to="/"
+                onClick={() => {
+                  setUser("");
+                }}
+              >
+                Logout
+              </NavLink>
+            </li>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink to="/signup">Signup</NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
