6cbd8b5b09606cb29c7df22f1e15ccb87ecae424
GemboJones <gembojones@gmail.com>
2024-04-21 04:57:17 +0100

    fix attempt

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,11 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+      <GoogleOAuthProvider
+        clientId={
+          "185919865548-g5apfncvvhiavig0i762fv922sfklhfe.apps.googleusercontent.com"
+        }
+      >
         <App />
       </GoogleOAuthProvider>
     </React.StrictMode>
