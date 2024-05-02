83968641d5bc2eec37253731ffc0634478b4899c
GemboJones <gembojones@gmail.com>
2024-05-02 13:34:19 +0100

    attempts my events page fix9

--- frontend/src/components/MyEvents.jsx
+++ frontend/src/components/MyEvents.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 export const MyEvents = () => {
   const { user } = useContext(UserContext);
 
-  const [myEvents, setMyEvents] = useState([]);
+  const [myEvents, setMyEvents] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -39,53 +39,53 @@ export const MyEvents = () => {
     <>
       <div className={styles.container}>
         <h1>My Events</h1>
-        {myEvents.length !== 0 ? (
-          <ul className={styles.eventsGrid}>
-            {myEvents.map(
-              ({
-                _id,
-                title,
-                topic,
-                startDate,
-                description,
-                location,
-                price,
-                image,
-                attending,
-              }) => {
-                return (
-                  <Link to={`/events/${_id}`} key={_id}>
-                    <li className={styles.eventCard}>
-                      <img
-                        src={image}
-                        alt=""
-                        className={styles.eventCard__image}
-                      />
-                      <div className={styles.eventCard__textContainer}>
-                        <p className={styles.topicText}>
-                          <strong>{topic}</strong>
-                        </p>
-                        <h3 className={styles.eventCard__title}>{title} </h3>
-                        <p>
-                          {Intl.DateTimeFormat("en-GB", dateOptions).format(
-                            Date.parse(startDate)
-                          )}
-                        </p>
-                        <p>📍 {location}</p>
-                      </div>
-                    </li>
-                  </Link>
-                );
-              }
-            )}
-          </ul>
-        ) : (
+        {!myEvents ? (
           <p>
             No events booked.{" "}
             <Link to={"/events"} className={styles.underlined}>
               Browse Events
             </Link>
           </p>
+        ) : (          
+            <ul className={styles.eventsGrid}>
+              {myEvents.map(
+                ({
+                  _id,
+                  title,
+                  topic,
+                  startDate,
+                  description,
+                  location,
+                  price,
+                  image,
+                  attending,
+                }) => {
+                  return (
+                    <Link to={`/events/${_id}`} key={_id}>
+                      <li className={styles.eventCard}>
+                        <img
+                          src={image}
+                          alt=""
+                          className={styles.eventCard__image}
+                        />
+                        <div className={styles.eventCard__textContainer}>
+                          <p className={styles.topicText}>
+                            <strong>{topic}</strong>
+                          </p>
+                          <h3 className={styles.eventCard__title}>{title} </h3>
+                          <p>
+                            {Intl.DateTimeFormat("en-GB", dateOptions).format(
+                              Date.parse(startDate)
+                            )}
+                          </p>
+                          <p>📍 {location}</p>
+                        </div>
+                      </li>
+                    </Link>
+                  );
+                }
+              )}
+            </ul>
         )}
       </div>
     </>
